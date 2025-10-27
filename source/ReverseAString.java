8c9e22360b4afca79c1d79eb90572849ba1b5338
ishritadutta <ishrita2025@gmail.com>
2025-10-27 12:15:58 +0530

    Reverse a String

---
+++ source/ReverseAString.java
@@ -0,0 +1,32 @@
+
+
+public class ReverseAString{
+
+    public static String Reverse(String str){
+
+    //Approach 1
+
+        /*StringBuilder rev = new StringBuilder(str).reverse();
+        System.out.println("Reverse str: "+rev);
+        */
+
+    // Approach 2
+        String rev ="";
+        for(int i=str.length()-1;i>=0;i--){
+        rev = rev+str.charAt(i);
+        //System.out.println(rev);
+        
+    }
+            return rev;
+    }
+
+    public static void main(String[] args) {
+
+        String str = "Ishrita";
+
+        System.out.println(Reverse(str));
+        
+       
+        
+    }
+}
