34af13073a75da4044757010db6afbf6939352e7
ishritadutta <ishrita2025@gmail.com>
2025-10-29 00:43:33 +0530

    Palindrome 2

--- StringProblems/Palindrome.java
+++ StringProblems/Palindrome.java
@@ -2,20 +2,30 @@ public class Palindrome{
 
     public static String IsPalindrome(String str){
         
+        //Approach 1
        String sb = new StringBuilder(str)
                         .reverse()
                         .toString();
 
-        System.out.println(str.equals(sb));
+        if(str.equals(sb)){
+        System.out.println("Palindrome");
+        }else{
+            System.out.println("Not Palindrome");
+        }
+        
+        
 
-        return "Palindrome";
+        return IsPalindrome(str+" is Plaindrome");
+
+        //Approch 2
+
     }
 
 
 
     public static void main(String[] args) {
         String str = "madam";
-        
+           
         
     }
 }
